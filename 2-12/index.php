d37fae6c12608145f5285e6f98ef6b05cf4fc098
arigatokeikaku <arigatokeikaku@gmail.com>
2023-02-18 10:52:34 +0900

    2-12再提出
    よろしくお願い致します。

--- 2-12/index.php
+++ 2-12/index.php
@@ -1,3 +1,5 @@
+<!doctype html>
+<html lang="ja">
 <form action = "result.php" method="post">
     お名前：<input type ="text" name ="my_name" /><br>
     ご希望商品：
@@ -5,20 +7,21 @@
     <input type="radio" name="keihin" value="B賞">B賞
     <input type="radio" name="keihin" value="C賞">C賞<br>
     個数：
-    <select name="個数">
+    <select name="kosu">
         <option value="1">1</option>
-        <option value="1">2</option>
-        <option value="1">3</option>
-        <option value="1">4</option>
-        <option value="1">5</option>
-        <option value="1">6</option>
-        <option value="1">7</option>
-        <option value="1">8</option>
-        <option value="1">9</option>
-        <option value="1">10</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
     </select>
     
     <input type="hidden" name="hidden_param" value="隠しパラメータから"　/>
 
     <input type ="submit" value ="送信するよ！" />
 </form>
+</html>
--- 2-12/result.php
+++ 2-12/result.php
@@ -1,10 +1,14 @@
+
+
 <?php
     $my_name = $_POST["my_name"];
     $keihin = $_POST["keihin"];
-    $kosu = $_POST["個数"];
+    $kosu = $_POST["kosu"];
 ?>
+<!doctype html>
+<html lang="ja">
 <p>お名前：<?php echo $my_name; ?></p>    
 <p>ご希望景品<?php echo $keihin; ?></p>    
 <p>個数：<?php echo $kosu; ?></p>    
 
-
+</html>
